6fb3d7bd934791e8862f3987f8273522eda47519
AntohaDontLookAtMe <powerwolfgdansk@gmail.com>
2025-08-31 18:42:30 +0400

    Задание 1

---
+++ WordSearcher/FolderFinder.cs
@@ -0,0 +1,59 @@
+﻿namespace WordSearcher
+{
+  /// <summary>
+  /// Класс для работы с папками.
+  /// </summary>
+  public static class FolderFinder
+  {
+    #region Методы
+
+    /// <summary>
+    /// Запрашивает у пользователя путь к папке с файлами.
+    /// </summary>
+    /// <returns>Путь к папке.</returns>
+    public static string GetFolderPath()
+    {
+      Console.Write("Введите полный путь к папке с файлами: ");
+      return Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Проверяет существование папки по указанному пути.
+    /// </summary>
+    /// <param name="path">Путь к файлу.</param>
+    /// <returns>true - папка существует, false - папки нет.</returns>
+    public static bool CheckFolderPath(string path)
+    {
+      if (!Directory.Exists(path))
+      {
+        Console.WriteLine($"Папка с путём {path} не существует.\n");
+        return false;
+      }
+
+      return true;
+    }
+
+    /// <summary>
+    /// Возвращает наличие файлов в папке и список файлов в ней.
+    /// </summary>
+    /// <param name="path">Путь к папке.</param>
+    /// <returns>
+    /// null,если в папке не оказалось файлов
+    /// Иначе пути к каждому файлу из папки.
+    /// </returns>
+    public static List<string> GetFilesFromFolder(string path)
+    {
+      var files = Directory.GetFiles(path).ToList();
+      if (files.Count == 0)
+      {
+        Console.WriteLine($"В папке {path} нет файлов.\n");
+        return null;
+      }
+
+      return files;
+    }
+
+     #endregion
+
+  }
+}
---
+++ WordSearcher/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WordSearcher
+{
+  /// <summary>
+  /// Класс запуска программы.
+  /// </summary>
+  internal class Program
+  {
+    static async Task Main(string[] args)
+    {
+      while (true)
+      {
+        string path = FolderFinder.GetFolderPath();
+
+        if (!FolderFinder.CheckFolderPath(path))
+        {
+          continue;
+        }
+
+        var files = FolderFinder.GetFilesFromFolder(path);
+        if (files == null)
+        {
+          continue;
+        }
+
+        string findedWord = WordFinder.GetFindedWord();
+
+        await SearchManager.SearchWordsInFiles(path, files, findedWord);
+      }
+    }
+  }
+}
